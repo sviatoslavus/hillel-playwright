39cc949efd9e979484308b5385f235e78a911095
sviatoslavus <sviatoslav.myhlias@gmail.com>
2024-07-07 18:58:38 +0300

    use environmental variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+
+dotenv.config({ path:  '.env'});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,10 +29,10 @@ export default defineConfig({
       testMatch: '**.qauto.spec.ts',
       use: {
         headless: false,
-        baseURL: 'https://qauto.forstudy.space/',
+        baseURL: process.env.BASE_URL,
         httpCredentials: {
-          username: 'guest',
-          password: 'welcome2qauto'
+          username: process.env.USER_NAME!,
+          password: process.env.USER_PASS!
         }
       }
     },
