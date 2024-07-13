4d6f54ca733fd4651192ee805dcbce926fa1a0a8
sviatoslavus <sviatoslav.myhlias@gmail.com>
2024-07-13 16:09:02 +0300

    add negative tests

--- tests/api/cars.qauto.spec.ts
+++ tests/api/cars.qauto.spec.ts
@@ -8,3 +8,24 @@ test('/cars/models public request', async ({ request }) => {
     const allCars = body.data;
     expect(response.status()).toBe(200)
 });
+test('get cars for current users', async ({request})=>{
+    const response = await request.get('/api/cars');
+    console.log(await response.json());
+})
+test('get 404 for not existing car id', async({request})=>{
+    const invalidId = 100
+    const response = await request.get(`/api/cars/models/${invalidId}`)
+    expect(response.status()).toBe(404)
+})
+test('user cant create car with invalid brand id', async ({request}) => {
+    const invalidId = 100
+    const response =  await request.post('/api/cars', {data:
+        {
+            "carBrandId": invalidId,
+            "carModelId": 1,
+            "mileage": 122
+          }
+    })
+    console.log(await response.json());
+    expect(response.status()).toBe(404)
+})
